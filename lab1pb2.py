4a1f936463de4a2ed9bd2e48a3456a0f052e5669
damaris-nicola <damaris.nicola@gmail.com>
2014-10-20 10:37:40 +0900

    english

--- lab1pb2.py
+++ lab1pb2.py
@@ -1,15 +1,26 @@
 # pentru un numar natural n dat gasiti numarul natural minim format cu aceleasi cifre
 
 
-def cifre_numar(n):
+def numbers_digit(n):
+    """
+    Extrage cifrele unui numar intr-o lista, si sorteaza lista crescator
+    :param n: int
+    :return:list
+    """
     L = []
     while (n > 0):
         L.append(n % 10)
         n = n // 10
+    L.sort()
     return L
 
 
-def formare_numar(L):
+def get_number(L):
+    """
+    Formeaza numarul folosind lista sortata si puterile lui 10
+    :param L: list
+    :return: int
+    """
     lungime = len(L)
     nr = 0
     for i in range(lungime):
@@ -20,7 +31,6 @@ if __name__ == "__main__":
     print("Dati numarul n dorit")
     n = int(input("n="))
     L = []
-    L = cifre_numar(n)
-    L.sort()
-    minim = formare_numar(L)
+    L = numbers_digit(n)
+    minim = get_number(L)
     print("Numarul minim format cu aceleasi cifre este: ", minim)
